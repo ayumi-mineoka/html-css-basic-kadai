ee0c914c8358447e5833fb1f4c3e6089d6c5af15
ayumi-mineoka <kocha14102024@gmail.com>
2025-07-23 14:01:51 +0900

    js課題３

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,13 +1,12 @@
-let num=15;
-if(num%3=== 0){
-    console.log("3の倍数です");
-}
-else if(num%5=== 0){ 
+let num = 15; // 1以上の正の数を代入
+
+if (num % 3 === 0 && num % 5 === 0) {
+  console.log("3と5の倍数です");
+} else if (num % 3 === 0) {
+  console.log("3の倍数です");
+} else if (num % 5 === 0) {
   console.log("5の倍数です");
+} else {
+  console.log(num);
 }
-else if(num%3=== 0 && num%5=== 0){
-    console.log("3と5の倍数です");
-}
-else{
-    console.log(num);
-}
+
